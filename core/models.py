8f1ca78e27b9395f5b8ff2578f6dd498e9583894
Benjamin D Adams <badams888@hotmail.com>
2020-06-02 20:15:45 -0400

    built models, played around with views

---
+++ core/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from users.models import User
+
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.tag
+
+# Create your models here.
+class CodeSnippet(models.Model):
+    body = models.TextField()
+    title = models.CharField(max_length=30)
+    tags = models.ManyToManyField(to=Tag, related_name='codesnippets')
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="codesnippets")
+    language = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_public = models.BooleanField(default=True)
+    parent = models.ForeignKey(to='self', on_delete=models.SET_NULL, related_name="children", null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import CodeSnippet
+from .models import Tag
+# Create your views here.
+
+
+# def load_homepage(request):
+    
+
+#     return render(request, )
